1bf143aec2bc182464f8dc40c4a1f8f4efe555d8
KevinPXu <kevinpxu21@gmail.com>
2022-09-28 17:58:26 -0500

    added solutions to 9.28 and completed activities from class

--- 03-javascript/activities/26-Stu_Object-This/Unsolved/script.js
+++ 03-javascript/activities/26-Stu_Object-This/Unsolved/script.js
@@ -1,22 +1,33 @@
+//console logs the global object
 console.log(this);
 
 function helloThis() {
-  console.log('Inside this function, this is ' + this);
+  // this is still referring to the global object even within the function
+  console.log("Inside this function, this is " + this);
 }
+helloThis();
 
 var child = {
   age: 10,
   ageTenYears: function () {
+    //console logs 10 + 10 or 20, calls self in object child
     console.log(this.age + 10);
+
+    console.log(this.age);
   },
 };
 
+child.ageTenYears();
+
 var investor = {
-  name: 'Cash Saver',
+  name: "Cash Saver",
   investment: {
     initialInvestment: 5000,
     investmentGrowth: function () {
-      console.log(this.initialInvestment * 1.15);
+      //console logs the initialInvestment or 5000 from the object this is in.
+      console.log(this.initialInvestment);
     },
   },
 };
+
+investor.investment.investmentGrowth();
