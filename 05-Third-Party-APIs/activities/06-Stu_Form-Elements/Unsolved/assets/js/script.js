f09b7b1f7b9289fbfacbe8d21e756e00b149e777
KevinPXu <kevinpxu21@gmail.com>
2022-10-04 12:01:33 -0500

    added solves from 10.3, and activites for 10.4

--- 05-Third-Party-APIs/activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,15 @@
-var shoppingFormEl = $('#shopping-form');
-var shoppingListEl = $('#shopping-list');
-
+var shoppingFormEl = $("#shopping-form");
+var shoppingListEl = $("#shopping-list");
+var input = $("#shopping-input");
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+
+function formSubmit(event) {
+  event.preventDefault();
+  var newLi = $("<li>");
+  newLi.text(input.val());
+  shoppingListEl.append(newLi);
+}
+
+$(shoppingFormEl).on("submit", formSubmit);
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
