6cc5ebe5e13fd8271199a64fb4c4a47e4c804c85
Kevin Xu <kevinpxu21@gmail.com>
2022-10-19 11:43:02 -0500

    added activites for 10.19

--- 09-NodeJS/activities/04-Stu_Arrow-Function-Practice/Solved/index.js
+++ 09-NodeJS/activities/04-Stu_Arrow-Function-Practice/Solved/index.js
@@ -1,9 +1,13 @@
 // 1. funnyCase makes each letter in a string the opposite case as the one before
-var funnyCase = (string) => {
+var funnyCase = string => {
   var newString = "";
   for (var i = 0; i < string.length; i++) {
-    if (i % 2 === 0) newString += string[i].toLowerCase();
-    else newString += string[i].toUpperCase();
+    if (i % 2 === 0) {
+      newString += string[i].toLowerCase();
+    }
+    else {
+      newString += string[i].toUpperCase();
+    }
   }
   return newString;
 };
@@ -18,16 +22,14 @@ var map = (arr, cb) => {
   var result = [];
   for (var index = 0; index < arr.length; index++) {
     var currentElement = arr[index];
-    result.push(cb(currentElement, index));
+    result.push(cb(currentElement));
   }
   return result;
 };
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var doubled = map(numbers, (element) => {
-  return element * 2;
-});
+var doubled = map(numbers, element => element * 2);
 
 // Prints `[ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ]`
 console.log(doubled);
@@ -39,16 +41,21 @@ var filter = (arr, cb) => {
   var result = [];
   for (var index = 0; index < arr.length; index++) {
     var currentElement = arr[index];
-    if (cb(currentElement, index)) {
+    if (cb(currentElement)) {
       result.push(currentElement);
     }
   }
   return result;
 };
 
-var evenNumbers = filter(numbers, (currentElement) => {
-  return currentElement % 2 === 0;
-});
+var evenNumbers = filter(numbers, currentElement => currentElement % 2 === 0);
+
+var allNumbers = filter(numbers, () => true);
+// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10;
+
+var noNumbers = filter(numbers, () => false);
+// []
+
 
 // Prints `[ 2, 4, 6, 8, 10 ]`
 console.log(evenNumbers);
@@ -61,22 +68,22 @@ var netflixQueue = {
     "Mr. Nobody",
     "The Matrix",
     "Eternal Sunshine of the Spotless Mind",
-    "Fight Club",
+    "Fight Club"
   ],
-  watchMovie: function () {
+  watchMovie: function() {
     this.queue.pop();
   },
-  addMovie: function (movie) {
+  addMovie: function(movie) {
     this.queue.unshift(movie);
   },
-  printQueue: function () {
+  printQueue: function() {
     var list = "";
     for (var i = this.queue.length - 1; i >= 0; i--) {
       var currentMovie = this.queue[i];
-      list += this.queue.length - i + ". " + currentMovie + "\n";
+      list += (this.queue.length - i) + ". " + currentMovie + "\n";
     }
     console.log(list);
-  },
+  }
 };
 
 console.log("Printing movie queue!\n");
--- 09-NodeJS/activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 09-NodeJS/activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,11 @@
 // music should be an object with title, artist, and album properties
 const music = {
-  // code here
- 
+  title: "Whats my Age",
+  artist: "Blink 182",
+  album: "Enema of the State",
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = `The title of the song is ${music.title} by ${music.artist}, in the album ${music.album}`;
 
-console.log(songSnippet)
-
+console.log(songSnippet);
