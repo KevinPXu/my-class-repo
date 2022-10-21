4b8df06c6005ef0e06aae24c66094b30891b3fe6
Kevin Xu <kevinpxu21@gmail.com>
2022-10-20 20:53:35 -0500

    added inquire prompts

--- 10-OOP/activities/06-Stu_Polymorphism/Unsolved/index.js
+++ 10-OOP/activities/06-Stu_Polymorphism/Unsolved/index.js
@@ -10,24 +10,47 @@ function Student(first, last, age) {
   this.displayGrade = function (grade) {
     const input = grade;
     if (!input) {
-      throw new Error('no grade provided');
+      throw new Error("no grade provided");
     }
     let response;
     // Return a letter grade if a number grade was passed
     // Ex. 95 => 'A'
-    if (typeof input === 'number') {
+    if (typeof input === "number") {
       // TODO: Add logic here to return a single letter grade
+      if (inRange(input, 90, 100)) {
+        response = "A";
+      } else if (inRange(input, 80, 89)) {
+        response = "B";
+      } else if (inRange(input, 70, 79)) {
+        response = "C";
+      } else if (inRange(input, 60, 69)) {
+        response = "D";
+      } else {
+        response = "F";
+      }
+
       return response;
     }
     // Return a range if a letter grade was passed
     // Ex. 'A' => '90 - 100'
-    if (typeof input === 'string') {
+    if (typeof input === "string") {
       // TODO: Add logic here to return range as a string
+      if ("A") {
+        response = "90-100";
+      } else if ("B") {
+        response = "80-89";
+      } else if ("C") {
+        response = "70-79";
+      } else if ("D") {
+        response = "60-69";
+      } else {
+        response = "<69";
+      }
       return response;
     }
   };
 }
 
-const John = new Student('John', 'Appleseed', '30');
-console.log('John.displayGrade():', John.displayGrade(95));
-console.log('John.displayGrade():', John.displayGrade('B'));
+const John = new Student("John", "Appleseed", "30");
+console.log("John.displayGrade():", John.displayGrade(95));
+console.log("John.displayGrade():", John.displayGrade("B"));
