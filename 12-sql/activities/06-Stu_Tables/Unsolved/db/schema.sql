27abd977897edb40d8b4ab29b976574c6c452cd0
Kevin Xu <kevinpxu21@gmail.com>
2022-10-28 11:39:32 -0500

    added files from 10.28

--- 12-sql/activities/06-Stu_Tables/Unsolved/db/schema.sql
+++ 12-sql/activities/06-Stu_Tables/Unsolved/db/schema.sql
@@ -3,4 +3,8 @@ DROP DATABASE IF EXISTS books_db;
 CREATE DATABASE books_db;
 USE books_db;
 
+CREATE TABLE biographies (
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL  
+);
 -- Add biography table --
