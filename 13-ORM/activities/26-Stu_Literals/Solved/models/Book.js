d058f30526445b36f4d8bc627e8a8a4a20dda2c0
Kevin Xu <kevinpxu21@gmail.com>
2022-11-03 11:38:13 -0500

    added activities for 11.3

--- 13-ORM/activities/26-Stu_Literals/Solved/models/Book.js
+++ 13-ORM/activities/26-Stu_Literals/Solved/models/Book.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Book extends Model {}
 
@@ -29,17 +29,17 @@ Book.init(
     },
     edition: {
       type: DataTypes.INTEGER,
-      defaultValue: 1
+      defaultValue: 1,
     },
     is_paperback: {
       type: DataTypes.BOOLEAN,
-      defaultValue: true
+      defaultValue: true,
     },
     reader_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'reader',
-        key: 'id',
+        model: "reader",
+        key: "id",
       },
     },
   },
@@ -48,7 +48,7 @@ Book.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'book'
+    modelName: "book",
   }
 );
 
