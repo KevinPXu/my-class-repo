4d6ced02706e1dda03b04b43f56adc1c9bbd9833
Kevin Xu <kevinpxu21@gmail.com>
2022-11-07 11:51:44 -0600

    added activities for 11.7

--- 14-MVC/activities/18-Stu_Cookies/Unsolved/server.js
+++ 14-MVC/activities/18-Stu_Cookies/Unsolved/server.js
@@ -1,30 +1,35 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 // Initializes Sequelize with session store
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Sets up session and connect to our Sequelize db
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   // TODO: Add a comment describing the purpose of adding a cookies object to our options to our session object
+  //cookies allow the server to store session information even if the browser is closed
   cookie: {
     // TODO: Add a comment describing the functionality of the maxAge attribute
+    //gives the cookie an expiration date
     maxAge: 3600,
     // TODO: Add a comment describing the functionality of the httpOnly attribute
+
     httpOnly: true,
     // TODO: Add a comment describing the functionality of the secure attribute
+    //makes it so the client cannnot access the information in the cookie
     secure: false,
     // TODO: Add a comment describing the functionality of the sameSite attribute
-    sameSite: 'strict',
+    //the cookie can only be used on the site it was created on
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
@@ -38,12 +43,12 @@ app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
